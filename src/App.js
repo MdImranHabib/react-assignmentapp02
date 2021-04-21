7a1710ad2cc7ed2ce9f5705a6e3cb662472d91bc
MdImranHabib <16103023@iubat.edu>
2021-04-21 12:29:23 +0600

    working with boostarp 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+      <div className = "row my-3 p-3 border">
+        <h2>This is Bootstarp 4</h2>
+        <p>Let's learn Bootstarp 4</p>
+      </div> 
+      <div className = "row my-3 p-3 bg-dark text-white">
+        <h2>This is Bootstarp 4</h2>
+        <p>Let's learn Bootstarp 4</p>
+      </div>
+      <div className = "row my-3 p-3 bg-primary">
+        <h2>This is Bootstarp 4</h2>
+        <p>Let's learn Bootstarp 4</p>
+      </div>     
     </div>
   );
 }
