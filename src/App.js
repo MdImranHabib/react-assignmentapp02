0a2bc05a96217a82a8116b7b16901db6f5f33e10
MdImranHabib <16103023@iubat.edu>
2021-04-22 13:06:40 +0600

    working with bootstrap 4 in react

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className = "container">
-      <div className = "row my-3 p-3 border">
-        <h2>This is Bootstarp 4</h2>
-        <p>Let's learn Bootstarp 4</p>
-      </div> 
-      <div className = "row my-3 p-3 bg-dark text-white">
-        <h2>This is Bootstarp 4</h2>
-        <p>Let's learn Bootstarp 4</p>
-      </div>
-      <div className = "row my-3 p-3 bg-primary">
-        <h2>This is Bootstarp 4</h2>
-        <p>Let's learn Bootstarp 4</p>
-      </div>     
+    <div>
+      <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <div className="container">
+          <a class="navbar-brand" href="#">SaRa</a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>    
+            </ul>
+          </div> 
+
+        </div>  
+      </nav>  
     </div>
   );
 }
